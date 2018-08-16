e67205fcde5a6687aca30f8c139bee56392b1a29
tfreitasleal <tfreitasleal@gmail.com>
2018-08-16 21:32:37 +0100

    Wisej release 1.5.12

--- Wisej.Web.Ext.OfficeViewer/Properties/AssemblyInfo.cs
+++ Wisej.Web.Ext.OfficeViewer/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Web;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.5.11.0")]
+[assembly: AssemblyFileVersion("1.5.12.0")]
