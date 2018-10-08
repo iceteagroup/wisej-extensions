5a6946b76d05bcb77f155f4360ac647fc231980b
Luca <gianluca@iceteagroup.com>
2018-10-08 19:37:04 -0400

    Added EditBox.

---
+++ Wisej.Web.Ext.EditBox/Platform/wisej.web.ext.EditBox.js
@@ -0,0 +1,51 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+//
+// (C) 2018 ICE TEA GROUP LLC - ALL RIGHTS RESERVED
+//
+// ALL INFORMATION CONTAINED HEREIN IS, AND REMAINS
+// THE PROPERTY OF ICE TEA GROUP LLC AND ITS SUPPLIERS, IF ANY.
+// THE INTELLECTUAL PROPERTY AND TECHNICAL CONCEPTS CONTAINED
+// HEREIN ARE PROPRIETARY TO ICE TEA GROUP LLC AND ITS SUPPLIERS
+// AND MAY BE COVERED BY U.S. AND FOREIGN PATENTS, PATENT IN PROCESS, AND
+// ARE PROTECTED BY TRADE SECRET OR COPYRIGHT LAW.
+//
+// DISSEMINATION OF THIS INFORMATION OR REPRODUCTION OF THIS MATERIAL
+// IS STRICTLY FORBIDDEN UNLESS PRIOR WRITTEN PERMISSION IS OBTAINED
+// FROM ICE TEA GROUP LLC.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * wisej.web.ext.EditBox
+ */
+qx.Class.define("wisej.web.ext.EditBox", {
+
+	extend: wisej.web.Control,
+
+	construct: function () {
+
+		this.base(arguments);
+	},
+
+	properties: {
+
+	},
+
+	members: {
+
+		// overridden
+		_createChildControlImpl: function (id, hash) {
+			var control;
+
+			switch (id) {
+
+				case "label":
+					// control = new wisej.web.ribbonBar.TabView();
+					// this.add(control);
+					break;
+			}
+
+			return control || this.base(arguments, id);
+		},
+	},
+});
