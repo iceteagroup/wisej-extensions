9725b77559e909b872d702014e6a5e07f6d5913a
Luca <gianluca@iceteagroup.com>
2018-11-01 19:31:12 -0400

    Added SideButton.

---
+++ Wisej.Web.Ext.SideButton/Platform/wisej.web.ext.SideButton.js
@@ -0,0 +1,127 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+//
+// (C) 2018 ICE TEA GROUP LLC - ALL RIGHTS RESERVED
+//
+// 
+//
+// ALL INFORMATION CONTAINED HEREIN IS, AND REMAINS
+// THE PROPERTY OF ICE TEA GROUP LLC AND ITS SUPPLIERS, IF ANY.
+// THE INTELLECTUAL PROPERTY AND TECHNICAL CONCEPTS CONTAINED
+// HEREIN ARE PROPRIETARY TO ICE TEA GROUP LLC AND ITS SUPPLIERS
+// AND MAY BE COVERED BY U.S. AND FOREIGN PATENTS, PATENT IN PROCESS, AND
+// ARE PROTECTED BY TRADE SECRET OR COPYRIGHT LAW.
+//
+// DISSEMINATION OF THIS INFORMATION OR REPRODUCTION OF THIS MATERIAL
+// IS STRICTLY FORBIDDEN UNLESS PRIOR WRITTEN PERMISSION IS OBTAINED
+// FROM ICE TEA GROUP LLC.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * wisej.web.ext.SideButton
+ * 
+ */
+qx.Class.define("wisej.web.ext.SideButton", {
+
+	extend: wisej.web.Button,
+
+	construct: function (text) {
+
+		this.base(arguments, text);
+
+		this._forwardStates.right = true;
+		this._forwardStates.left = true;
+		this._forwardStates.collapsed = true;
+	},
+
+	properties: {
+
+		// overridden.
+		appearance: { init: "side-button", refine: true },
+
+		/**
+		 *  Collapsed property.
+		 */
+		collapsed: { init: true, check: "Boolean", apply: "_applyCollapsed" },
+
+		/**
+		 *  Alignment property.
+		 *  
+		 *  Indicates the direction the button "retracts" to. When aligned to the right, the button
+		 *  will retracts to the right end. When aligned to the left, it will retract to the left side.
+		 */
+		alignment: { init: "left", check: ["left", "right"], apply: "_applyAlignment" }
+
+	},
+
+	members: {
+
+		/**
+		 * Applies the Collapsed property.
+		 */
+		_applyCollapsed: function (value, old) {
+
+			if (value) {
+				this.addState("collapsed");
+				this.setUserData("expandedWidth", this.getWidth());
+
+				if (!this.hasState("hovered"))
+					this.resetWidth();
+			}
+			else {
+				this.removeState("collapsed");
+			}
+		},
+
+		/**
+		 * Applies the Alignment property.
+		 */
+		_applyAlignment: function (value, old) {
+
+			if (old)
+				this.removeState(old);
+			if (value)
+				this.addState(value);
+		},
+
+		_onPointerOver: function (e) {
+
+			this.base(arguments, e);
+
+			if (this.isCollapsed()) {
+				this.setWidth(this.getUserData("expandedWidth"));
+			}
+		},
+
+		_onPointerOut: function (e) {
+
+			this.base(arguments, e);
+
+			if (this.isCollapsed()) {
+				this.resetWidth();
+			}
+		},
+
+		// overridden
+		renderLayout: function (left, top, width, height) {
+
+			var changes = this.base(arguments, left, top, width, height);
+
+			if (this.getAlignment() === "right" && this.isCollapsed()) {
+
+				var parentBounds = this.getLayoutParent().getBounds();
+				var rightMargin = parentBounds.width - left - this.getUserData("expandedWidth");
+
+				var el = this.getContentElement();
+				el.setStyles({
+					left: null,
+					right: "0px",
+					marginRight: rightMargin + "px"
+				});
+
+			}
+		}
+
+	}
+
+});
---
+++ Wisej.Web.Ext.SideButton/SideButton.cs
@@ -0,0 +1,138 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+//
+// (C) 2018 ICE TEA GROUP LLC - ALL RIGHTS RESERVED
+//
+// 
+//
+// ALL INFORMATION CONTAINED HEREIN IS, AND REMAINS
+// THE PROPERTY OF ICE TEA GROUP LLC AND ITS SUPPLIERS, IF ANY.
+// THE INTELLECTUAL PROPERTY AND TECHNICAL CONCEPTS CONTAINED
+// HEREIN ARE PROPRIETARY TO ICE TEA GROUP LLC AND ITS SUPPLIERS
+// AND MAY BE COVERED BY U.S. AND FOREIGN PATENTS, PATENT IN PROCESS, AND
+// ARE PROTECTED BY TRADE SECRET OR COPYRIGHT LAW.
+//
+// DISSEMINATION OF THIS INFORMATION OR REPRODUCTION OF THIS MATERIAL
+// IS STRICTLY FORBIDDEN UNLESS PRIOR WRITTEN PERMISSION IS OBTAINED
+// FROM ICE TEA GROUP LLC.
+//
+///////////////////////////////////////////////////////////////////////////////
+///
+using System.ComponentModel;
+using System.Drawing;
+using Wisej.Base;
+using Wisej.Core;
+
+namespace Wisej.Web.Ext.SideButton
+{
+	/// <summary>
+	/// Represents a retractable animated
+	/// button that can be used expand or collapse other panels.
+	/// </summary>
+	[ToolboxItem(true)]
+	[ToolboxBitmap(typeof(SideButton))]
+	[Description("Retractable animated side button.")]
+	public class SideButton : Button, IWisejControl
+	{
+		#region Constructor
+
+		public SideButton()
+		{
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Returns or sets the alignment and direction of the <see cref="SideButton"/>.
+		/// </summary>
+		[DefaultValue(LeftRightAlignment.Left)]
+		[SRCategory("CatBehavior")]
+		[Description("Returns or sets the alignment and direction of the SideButton.")]
+		public LeftRightAlignment Alignment
+		{
+			get
+			{
+				return this._alignment;
+			}
+			set
+			{
+				if (this._alignment != value)
+				{
+					this._alignment = value;
+					Update();
+				}
+			}
+		}
+		private LeftRightAlignment _alignment;
+
+		/// <summary>
+		/// Returns or sets the collapsed state of the <see cref="SideButton"/>
+		/// </summary>
+		[DefaultValue(false)]
+		[SRCategory("CatBehavior")]
+		[Description("Returns or sets the collapsed state of the SideButton.")]
+		public bool Collapsed
+		{
+			get
+			{
+				return this._collapsed;
+			}
+			set
+			{
+				if (this._collapsed != value)
+				{
+					this._collapsed = value;
+					Update();
+				}
+			}
+		}
+		private bool _collapsed;
+
+		private int ThemeHeight
+		{
+			get
+			{
+				var state = this.Collapsed ? "collapsed" : null;
+				return Application.Theme.GetProperty<int>(
+					((IWisejControl)this).AppearanceKey, "height", state);
+			}
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Returns the theme appearance key for this control.
+		/// </summary>
+		string IWisejControl.AppearanceKey
+		{
+			get
+			{
+				return
+					this.AppearanceKey ?? "side-button";
+			}
+		}
+		protected override void SetBoundsCore(int x, int y, int width, int height, BoundsSpecified specified)
+		{
+			height = this.ThemeHeight;
+			base.SetBoundsCore(x, y, width, height, specified);
+		}
+
+		#endregion
+
+		#region Wisej Implementation
+
+		protected override void OnWebRender(dynamic config)
+		{
+			base.OnWebRender((object)config);
+
+			config.className = "wisej.web.ext.SideButton";
+			config.collapsed = this.Collapsed;
+			config.alignment = this.Alignment;
+		}
+
+		#endregion
+	}
+}
