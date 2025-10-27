e98c1b253757b93575f259ca397a296c792fe8be
Frank <frank@iceteagroup,com>
2025-10-27 22:49:46 +0100

    3.5.24

---
+++ Wisej.Web.Ext.FullCalendar/EventMouseLeaveArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wisej.Web.Ext.FullCalendar
+{
+	public class EventMouseLeaveArgs : MouseEventArgs
+	{
+		/// <summary>
+		/// Constructs a new instance of <see cref="T:Wisej.Web.Ext.FullCalendar.EventMouseLeaveArgs"/>.
+		/// </summary>
+		/// <param name="button">One of the <see cref="T:Wisej.Web.MouseButtons"/> values that indicate which mouse button was pressed.</param>
+		/// <param name="location">The location of a pointer click, in pixels.</param>
+		public EventMouseLeaveArgs(Event ev, MouseButtons button, Point location)
+			: base(button, 1, location.X, location.Y, 0)
+		{
+			this.Event = ev;
+		}
+
+		/// <summary>
+		/// Returns the <see cref="T:Wisej.Web.Ext.FullCalendar.Event"/> that was clicked in the <see cref="T:Wisej.Web.Ext.FullCalendar.FullCalendar"/> control.
+		/// </summary>
+		public Event Event
+		{
+			get;
+			private set;
+		}
+	}
+}
--- Wisej.Web.Ext.JustGage/README.md
+++ Wisej.Web.Ext.JustGage/README.md
@@ -5,7 +5,7 @@ JustGage
 
 The JustGage extension component uses the cool JustGage widget to render animated, nice and clean gauges. It is based on Raphaël library for vector drawing, so it’s completely resolution independent and self-adjusting. As a Wisej Real Time widget you can set several properties: value, label, colors, range and more.
 
-Uses 3rd party JavaScript library [JustGage](http://justgage.com/)
+Uses 3rd party JavaScript library [JustGage](https://github.com/toorshia/justgage)
 
 License
 -------
