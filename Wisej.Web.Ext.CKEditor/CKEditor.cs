9d2f6d9106e563dedca8eec2c64ff6cfd4293632
Luca <luca.pivato@iceteagroup.com>
2025-12-31 14:06:13 +0100

    https://github.com/iceteagroup/Wisej/issues/3643

--- Wisej.Web.Ext.CKEditor/CKEditor.cs
+++ Wisej.Web.Ext.CKEditor/CKEditor.cs
@@ -426,7 +426,6 @@ namespace Wisej.Web.Ext.CKEditor
 			get { return _baseUrl; }
 			set { _baseUrl = value; }
 		}
-
 		private static string _baseUrl = "https://cdn.ckeditor.com/4.21.0/full-all/";
 
 		/// <summary>
