eb40c130bf0cf4e40d9b1571ecbc9f63df72b052
Frank <frank@iceteagroup,com>
2026-01-17 22:39:17 +0100

    3.5.26 (version infos)

--- Wisej.Ext.ClearScript/Properties/AssemblyInfo.cs
+++ Wisej.Ext.ClearScript/Properties/AssemblyInfo.cs
@@ -47,7 +47,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("3.5.26.11")]
+[assembly: AssemblyFileVersion("3.5.26.13")]
 
 namespace Microsoft.ClearScript.Properties
 {
