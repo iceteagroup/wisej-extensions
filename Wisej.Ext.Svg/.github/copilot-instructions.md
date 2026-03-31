60cf586610e0606bc8f9b3caac982635fbce9e49
Alaa Trigui <57464198+AlaaTrAce@users.noreply.github.com>
2026-03-31 13:11:58 +0100

    FluentUI IconPacks + NuSpec files + https://github.com/iceteagroup/Wisej-QA/issues/2405

---
+++ Wisej.Ext.Svg/.github/copilot-instructions.md
@@ -0,0 +1,5 @@
+# Copilot Instructions
+
+## General Guidelines
+- Use qooxdoo/Wisej code style with tab indentation and explicit `function (...)` member syntax (class-style structure similar to qx.Class.define examples).
+- For qooxdoo image patches, prefer applying SVG updates via `this.setSource(...)` and avoid manual DOM image manipulation/replacement.
---
+++ Wisej.Ext.Svg/Platform/qx.ui.basic.Image.patch.js
@@ -0,0 +1,120 @@
+var __wisejImageOriginalApplySource = qx.ui.basic.Image.prototype._applySource;
+
+qx.Mixin.define("wisej.web.qx.ui.basic.Imagepatch", {
+
+	statics : {
+		__svgCache : {}
+	},
+
+	members : {
+
+		__svgReqId : 0,
+
+		_extractColorFromSource : function(source) {
+
+			if (!source)
+				return null;
+
+			var queryColorIndex = source.indexOf("?color=");
+			if (queryColorIndex !== -1)
+				return source.substring(queryColorIndex + 7);
+
+			var hashIndex = source.indexOf("#");
+			if (hashIndex !== -1)
+				return source.substring(hashIndex);
+
+			return null;
+		},
+
+		_stripColorFromSource : function(source) {
+
+			if (!source)
+				return source;
+
+			var hashIndex = source.indexOf("#");
+			if (hashIndex !== -1)
+				source = source.substring(0, hashIndex);
+
+			return source;
+		},
+
+		_applySource : function(value, old) {
+
+			this.__svgReqId++;
+
+			if (!value || value.indexOf(".svg") === -1) {
+				__wisejImageOriginalApplySource.call(this, value, old);
+				this.__forceUpdate();
+				return;
+			}
+
+			var reqId = this.__svgReqId;
+			var source = this._stripColorFromSource(value);
+			var originalSource = value;
+
+			var color = this._extractColorFromSource(value);
+			if (!color && this.getTextColor()) {
+				var rgb = qx.util.ColorUtil.stringToRgb(this.getTextColor());
+				color = qx.util.ColorUtil.rgbToHexString(rgb);
+			}
+
+			var cacheKey = source + "|" + (color || "");
+			var cached = wisej.web.qx.ui.basic.Imagepatch.__svgCache[cacheKey];
+
+			if (cached) {
+				this.setSource(cached);
+				this.__forceUpdate();
+				return;
+			}
+
+			var me = this;
+			Snap.load(source, function(svg) {
+
+				if (reqId !== me.__svgReqId || me.isDisposed())
+					return;
+
+				if (me.getSource() !== originalSource)
+					return;
+
+				if (!svg || !svg.node) {
+					__wisejImageOriginalApplySource.call(me, source, old);
+					me.__forceUpdate();
+					return;
+				}
+
+				var node = svg.node;
+				node.setAttribute("width", "100%");
+				node.setAttribute("height", "100%");
+				node.setAttribute("preserveAspectRatio", "xMidYMid meet");
+
+				if (color)
+					node.style.color = color;
+
+				var dataUri = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(node.outerHTML);
+				wisej.web.qx.ui.basic.Imagepatch.__svgCache[cacheKey] = dataUri;
+
+				if (reqId !== me.__svgReqId || me.isDisposed())
+					return;
+
+				if (me.getSource() !== originalSource)
+					return;
+
+				me.setSource(dataUri);
+				me.__forceUpdate();
+			});
+		},
+
+		__forceUpdate : function() {
+
+			if (this.isDisposed())
+				return;
+
+			this.invalidateLayoutCache();
+			qx.ui.core.queue.Layout.add(this);
+			qx.ui.core.queue.Widget.add(this);
+
+		}
+	}
+});
+
+qx.Class.patch(qx.ui.basic.Image, wisej.web.qx.ui.basic.Imagepatch);
