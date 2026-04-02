85e546da263ef7acdfbbd9c30998fd225d4c269c
GitHub Action <action@github.com>
2026-04-02 21:39:35 +0000

    ci: update FileVersion to 3.5.32.3 and ReleaseDate to 04/02/2026

--- Wisej.Web.Ext.OnlyOffice/Properties/AssemblyInfo.cs
+++ Wisej.Web.Ext.OnlyOffice/Properties/AssemblyInfo.cs
@@ -23,4 +23,4 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.5.31.21")]
+[assembly: AssemblyFileVersion("3.5.32.3")]
