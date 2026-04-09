1891aca92773e69949879d0e5f8db82efdac1539
GitHub Action <action@github.com>
2026-04-09 16:20:29 +0000

    ci: update FileVersion to 4.1.0.14 and ReleaseDate to 04/09/2026

--- Wisej.Ext.ClearScript/Properties/AssemblyInfo.cs
+++ Wisej.Ext.ClearScript/Properties/AssemblyInfo.cs
@@ -47,7 +47,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.1.0.13")]
+[assembly: AssemblyFileVersion("4.1.0.14")]
 
 namespace Microsoft.ClearScript.Properties
 {
