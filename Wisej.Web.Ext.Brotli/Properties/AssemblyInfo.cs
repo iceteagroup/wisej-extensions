00f0680e72ec41e74e36195e023cb9ef49b758b9
GitHub Action <action@github.com>
2026-04-09 14:05:06 +0000

    ci: update FileVersion to 4.1.0.12 and ReleaseDate to 04/09/2026

--- Wisej.Web.Ext.Brotli/Properties/AssemblyInfo.cs
+++ Wisej.Web.Ext.Brotli/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using Wisej.Core;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.1.0.11")]
+[assembly: AssemblyFileVersion("4.1.0.12")]
