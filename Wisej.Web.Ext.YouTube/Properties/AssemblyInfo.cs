ee659b117a0b65d8066fc36aad87404948c540a9
GitHub Action <action@github.com>
2026-04-17 23:22:01 +0000

    ci: update FileVersion to 4.0.12.20260417 and ReleaseDate to 04/17/2026

--- Wisej.Web.Ext.YouTube/Properties/AssemblyInfo.cs
+++ Wisej.Web.Ext.YouTube/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using Wisej.Core;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.12.20260416")]
+[assembly: AssemblyFileVersion("4.0.12.20260417")]
