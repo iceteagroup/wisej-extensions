57a4249106fa948fa90cddfeb62f2f459d7f23f1
GitHub Action <action@github.com>
2026-04-18 22:17:01 +0000

    ci: update FileVersion to 3.5.31.20260418 and ReleaseDate to 04/18/2026

--- Wisej.Web.Ext.ApexCharts/Properties/AssemblyInfo.cs
+++ Wisej.Web.Ext.ApexCharts/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.5.32.6")]
+[assembly: AssemblyFileVersion("3.5.31.20260418")]
