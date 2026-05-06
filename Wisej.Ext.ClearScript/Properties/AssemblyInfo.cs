0398820a2d0afafac8425725294e3fe2ff419bfa
GitHub Action <action@github.com>
2026-05-06 23:48:22 +0000

    ci: update FileVersion to 3.5.33.7 and ReleaseDate to 05/06/2026

--- Wisej.Ext.ClearScript/Properties/AssemblyInfo.cs
+++ Wisej.Ext.ClearScript/Properties/AssemblyInfo.cs
@@ -47,7 +47,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("3.5.33.5")]
+[assembly: AssemblyFileVersion("3.5.33.7")]
 
 namespace Microsoft.ClearScript.Properties
 {
--- Wisej.Web.Ext.ApexCharts/Properties/AssemblyInfo.cs
+++ Wisej.Web.Ext.ApexCharts/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.5.33.5")]
+[assembly: AssemblyFileVersion("3.5.33.7")]
