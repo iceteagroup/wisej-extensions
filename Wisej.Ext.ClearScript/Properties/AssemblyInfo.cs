9604777f1239127d43314f90cfae9f46e57eccde
GitHub Action <action@github.com>
2026-05-12 14:25:26 +0000

    ci: update FileVersion to 3.5.33.9 and ReleaseDate to 05/12/2026

--- Wisej.Ext.ClearScript/Properties/AssemblyInfo.cs
+++ Wisej.Ext.ClearScript/Properties/AssemblyInfo.cs
@@ -47,7 +47,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("3.5.33.8")]
+[assembly: AssemblyFileVersion("3.5.33.9")]
 
 namespace Microsoft.ClearScript.Properties
 {
