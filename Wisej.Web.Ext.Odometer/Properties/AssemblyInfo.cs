be4a47306bb72077373286405d14bb206e3a4f5f
GitHub Action <action@github.com>
2026-05-13 13:49:00 +0000

    ci: update FileVersion to 4.1.0.28 and ReleaseDate to 05/13/2026

--- Wisej.Web.Ext.Odometer/Properties/AssemblyInfo.cs
+++ Wisej.Web.Ext.Odometer/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using Wisej.Core;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.1.0.27")]
+[assembly: AssemblyFileVersion("4.1.0.28")]
