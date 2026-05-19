61f48270d509a3dc9f8be42a6ff04dad7e4385ce
GitHub Action <action@github.com>
2026-05-19 22:40:49 +0000

    ci: update FileVersion to 4.1.0.29 and ReleaseDate to 05/19/2026

--- Wisej.Web.Ext.Mermaid/Properties/AssemblyInfo.cs
+++ Wisej.Web.Ext.Mermaid/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.1.0.28")]
+[assembly: AssemblyFileVersion("4.1.0.29")]
 
