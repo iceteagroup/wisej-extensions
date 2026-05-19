d386d921febff27433ff53a0696705420fa5e0e5
GitHub Action <action@github.com>
2026-05-19 23:42:43 +0000

    ci: update FileVersion to 4.1.0.31 and ReleaseDate to 05/19/2026

--- Wisej.Web.Ext.QuillJS/Properties/AssemblyInfo.cs
+++ Wisej.Web.Ext.QuillJS/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.1.0.30")]
+[assembly: AssemblyFileVersion("4.1.0.31")]
