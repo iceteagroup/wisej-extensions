9eb4605ad9f352e1ccf1394af86abc8a1c2ee045
GitHub Action <action@github.com>
2026-05-20 20:06:47 +0000

    ci: update FileVersion to 3.5.33.2 and ReleaseDate to 05/20/2026

--- Wisej.Web.Ext.SideButton/Properties/AssemblyInfo.cs
+++ Wisej.Web.Ext.SideButton/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using Wisej.Core;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.5.33.10")]
+[assembly: AssemblyFileVersion("3.5.33.2")]
