4957fb40045970730e7c7905732e6c4b52ee72e9
GitHub Action <action@github.com>
2026-05-30 22:56:18 +0000

    ci: update FileVersion to 4.0.14.1 and ReleaseDate to 05/30/2026

--- Wisej.Web.Ext.CoolClock/Properties/AssemblyInfo.cs
+++ Wisej.Web.Ext.CoolClock/Properties/AssemblyInfo.cs
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.1.0.18")]
+[assembly: AssemblyFileVersion("4.0.14.1")]
