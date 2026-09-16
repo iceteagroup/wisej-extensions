121e609907f92b1df4f04a9f9f4adf4b59eda6a0
JulieBodette <jr3python@gmail.com>
2026-09-16 12:38:54 -0400

    Fix AutoSizeLabel in ChatControl so that it does not cut off images
    https://github.com/iceteagroup/Wisej-QA/issues/2770

--- Wisej.Web.Ext.ChatControl/AutoSizeLabel.cs
+++ Wisej.Web.Ext.ChatControl/AutoSizeLabel.cs
@@ -26,7 +26,122 @@ namespace Wisej.Web.Ext.ChatControl
 	/// </summary>
 	public class AutoSizeLabel : Label
 	{
-		private Size _maxSize;
+        #region Client Implementation
+
+        // Client side script that resizes the label when the HTML text contains images.
+        //
+        // The label measures its HTML text on the client using a hidden element. The
+        // browser reports a size of 0x0 for the images that haven't been downloaded yet,
+        // making the label too small and cutting off the images.
+        //
+        // This script preloads the images, writes their size (scaled down to fit the
+        // maximum width of the label, when set) back to the <img> tags and re-measures
+        // the label. Once the <img> tags declare their size, the measurement is correct
+        // regardless of whether the images have been downloaded.
+        private const string AutoSizeImagesScript = @"
+if (!this.__autoSizeImages) {
+
+	// Returns the maximum width available to the images, 0 when unlimited.
+	this.__getImageMaxWidth = function () {
+
+		var maxWidth = this.getMaxWidth();
+		if (!maxWidth)
+			return 0;
+
+		var insets = this.getInsets();
+		return Math.max(0, maxWidth - insets.left - insets.right);
+	};
+
+	// Preloads the images in the HTML text and writes back their size.
+	this.__autoSizeImages = function () {
+
+		var html = this.getValue();
+		if (!html || html.indexOf('<img') === -1)
+			return;
+
+		// parse the HTML text to find the images that haven't been measured yet.
+		var container = document.createElement('div');
+		container.innerHTML = html;
+
+		var images = container.getElementsByTagName('img');
+		var pending = [];
+
+		for (var i = 0; i < images.length; i++) {
+			if (images[i].getAttribute('data-autosize') !== 'true')
+				pending.push(images[i]);
+		}
+
+		if (pending.length === 0)
+			return;
+
+		var me = this;
+		var count = pending.length;
+		var maxWidth = this.__getImageMaxWidth();
+
+		// Called when an image has been loaded (or failed to load).
+		// Updates the HTML text when all the images have been processed.
+		var update = function () {
+
+			if (--count > 0)
+				return;
+
+			var changed = false;
+
+			for (var i = 0; i < pending.length; i++) {
+
+				var image = pending[i];
+				var size = qx.io.ImageLoader.getSize(image.src);
+
+				// couldn't load the image, leave it alone.
+				if (!size || !size.width || !size.height)
+					continue;
+
+				// keep the width requested in the HTML text, if any.
+				var width = parseInt(image.getAttribute('width'), 10);
+				if (!(width > 0))
+					width = size.width;
+
+				var height = Math.round(size.height * width / size.width);
+
+				// scale the image down to fit the width of the label.
+				if (maxWidth > 0 && width > maxWidth) {
+					height = Math.round(height * maxWidth / width);
+					width = maxWidth;
+				}
+
+				image.setAttribute('width', width);
+				image.setAttribute('height', height);
+				image.setAttribute('data-autosize', 'true');
+
+				changed = true;
+			}
+
+			if (!changed)
+				return;
+
+			// update the HTML text and measure the label again now
+			// that the size of the images is known.
+			me.setValue(container.innerHTML);
+			me.invalidateLayoutCache();
+			qx.ui.core.queue.Layout.add(me);
+		};
+
+		for (var i = 0; i < pending.length; i++)
+			qx.io.ImageLoader.load(pending[i].src, update);
+	};
+
+	// process the images every time the HTML text changes.
+	this.addListener('changeValue', function () {
+		this.__autoSizeImages();
+	}, this);
+
+	this.__autoSizeImages();
+}
+";
+
+        #endregion
+
+        private Size _maxSize;
 
 		/// <summary>
 		/// Initializes a new instance of <see cref="AutoSizeLabel"/>.
@@ -43,7 +158,19 @@ namespace Wisej.Web.Ext.ChatControl
 			return base.GetPreferredSize(proposedSize);
 		}
 
-		protected override void OnWebRender(dynamic config)
+        /// <summary>
+        /// Installs the client side script that resizes the label
+        /// when the HTML text contains images.
+        /// </summary>
+        protected override void OnCreateControl()
+        {
+            base.OnCreateControl();
+
+            if (!this.DesignMode)
+                Eval(AutoSizeImagesScript);
+        }
+
+        protected override void OnWebRender(dynamic config)
 		{
 			base.OnWebRender((object)config);
 
